4c0d07f923f6bd38b8d63f1476549e0dd4bc03f0
François-Xavier Bourlet <bombela@uber.com>
2017-05-03 17:46:46 -0700

    wip

---
+++ internal/debug/debug.go
@@ -0,0 +1,155 @@
+// Copyright (c) 2017 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package debug
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+
+	"go.uber.org/yarpc/internal/introspection"
+)
+
+type HTTPMux interface {
+	Handle(pattern string, handler http.Handler)
+	HandleFunc(pattern string, handler func(http.ResponseWriter, *http.Request))
+}
+
+type IntrospectionProvider interface {
+	Dispatchers() []introspection.DispatcherStatus
+	DispatchersByName(name string) []introspection.DispatcherStatus
+	PackageVersions() []introspection.PackageVersion
+}
+
+type page struct {
+	path    string
+	handler func(w http.ResponseWriter, req *http.Request,
+		is IntrospectionProvider) interface{}
+	html string
+	tmpl *template.Template
+}
+
+type DebugPages struct {
+	is    IntrospectionProvider
+	pages map[string]*page
+}
+
+func (dp *DebugPages) registerPage(page *page) {
+	// We do not .Clone() the base template, but reparse it every time. Because
+	// of a race condition/memory leak when template.Clone() is used in
+	// conjunction with template blocks on Go<=1.8.
+	base := template.Must(template.New("base").Parse(baseHTML))
+	page.tmpl = template.Must(base.Parse(page.html))
+	dp.pages[page.path] = page
+}
+
+func (dp *DebugPages) executePage(w http.ResponseWriter, req *http.Request, page *page) {
+	data := page.handler(w, req, dp.is)
+	if data == nil {
+		return
+	}
+	if err := page.tmpl.Execute(w, data); err != nil {
+		fmt.Fprintf(w, "Failed executing template: %v", err)
+		return
+	}
+}
+
+func (dp *DebugPages) newPageHandler(page *page) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		dp.executePage(w, req, page)
+	}
+}
+
+func NewDebugPages(is IntrospectionProvider) *DebugPages {
+	r := DebugPages{
+		is:    is,
+		pages: make(map[string]*page),
+	}
+	r.registerPage(&rootPage)
+	r.registerPage(&idlPage)
+	return &r
+}
+
+func (dp *DebugPages) RegisterOn(mux HTTPMux) {
+	for _, page := range dp.pages {
+		mux.HandleFunc(page.path, dp.newPageHandler(page))
+	}
+}
+
+const baseHTML = `
+<html>
+	<head>
+		<title>{{ block "title" . }}yarpc{{ end }}</title>
+		<style type="text/css">
+			body {
+				font-family: "Courier New", Courier, monospace;
+			}
+			table {
+				color:#333333;
+				border-width: 1px;
+				border-color: #3A3A3A;
+				border-collapse: collapse;
+			}
+			table th {
+				border-width: 1px;
+				padding: 8px;
+				border-style: solid;
+				border-color: #3A3A3A;
+				background-color: #B3B3B3;
+			}
+			table td {
+				border-width: 1px;
+				padding: 8px;
+				border-style: solid;
+				border-color: #3A3A3A;
+				background-color: #ffffff;
+			}
+			header::after {
+				content: "";
+				clear: both;
+				display: table;
+			}
+			h1 {
+				width: 40%;
+				float: left;
+				margin: 0;
+			}
+			div.dependencies {
+				width: 60%;
+				float: left;
+				font-size: small;
+				text-align: right;
+			}
+		</style>
+	</head>
+	<body>
+		<header>
+		<h1>{{ template "title" . }}</h1>
+		<div class="dependencies">
+			{{range .PackageVersions}}
+			<span>{{.Name}}={{.Version}}</span>
+			{{end}}
+		</div>
+		</header>
+		{{ block "body" . }}Something here{{ end }}
+	</body>
+</html>
+`
---
+++ internal/debug/debug_idl.go
@@ -0,0 +1,143 @@
+// Copyright (c) 2017 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package debug
+
+import (
+	"fmt"
+	"net/http"
+	"sort"
+	"strings"
+
+	"go.uber.org/yarpc/internal/introspection"
+)
+
+type dispatcherIdl struct {
+	Name       string
+	ID         string
+	IDLModules []introspection.IDLModule
+}
+
+var idlPage = page{
+	path: "/debug/yarpc/idl/",
+	handler: func(w http.ResponseWriter, req *http.Request, is IntrospectionProvider) interface{} {
+		path := strings.TrimPrefix(req.URL.Path, "/debug/yarpc/idl/")
+		parts := strings.SplitN(path, "/", 2)
+		var selectDispatcher string
+		var selectIDL string
+		if path != "" {
+			if len(parts) != 2 {
+				w.WriteHeader(400)
+				fmt.Fprintf(w, "Invalid arguments")
+				return nil
+			}
+			selectDispatcher = parts[0]
+			selectIDL = parts[1]
+		}
+
+		var dispatchers []introspection.DispatcherStatus
+		if selectDispatcher != "" {
+			dispatchers = is.DispatchersByName(selectDispatcher)
+
+			if len(dispatchers) == 0 {
+				w.WriteHeader(404)
+				fmt.Fprintf(w, "dispatcher %q not found", selectDispatcher)
+				return nil
+			}
+		} else {
+			dispatchers = is.Dispatchers()
+		}
+
+		data := struct {
+			Dispatchers     []dispatcherIdl
+			PackageVersions []introspection.PackageVersion
+		}{
+			PackageVersions: is.PackageVersions(),
+		}
+
+		for _, d := range dispatchers {
+			idls := d.Procedures.IDLModules()
+			if selectIDL != "" {
+				var selected []introspection.IDLModule
+				for i, idl := range idls {
+					if idl.FilePath == selectIDL {
+						selected = idls[i : i+1]
+						break
+					}
+				}
+				idls = selected
+			} else {
+				sort.Sort(idls)
+			}
+			data.Dispatchers = append(data.Dispatchers, dispatcherIdl{
+				Name:       d.Name,
+				ID:         d.ID,
+				IDLModules: idls,
+			})
+		}
+
+		if selectDispatcher == "" {
+			return data
+		}
+
+		for idx, d := range data.Dispatchers {
+			if len(data.Dispatchers) > 1 {
+				fmt.Fprintf(w, "Dispatcher %q #%d:\n", d.Name, idx)
+			}
+			if len(d.IDLModules) != 1 {
+				if len(data.Dispatchers) == 1 {
+					w.WriteHeader(404)
+				}
+				fmt.Fprintf(w, "IDL not found: %q\n", selectIDL)
+				continue
+			}
+			fmt.Fprintf(w, d.IDLModules[0].RawContent)
+		}
+		return nil
+	},
+	html: `
+{{ define "title"}}/debug/yarpc/idl{{ end }}
+{{ define "body" }}
+{{range .Dispatchers}}
+	<hr />
+	<h2>Dispatcher "{{.Name}}" <small>({{.ID}})</small></h2>
+	<table>
+		<tr>
+			<th>File</th>
+			<th>SHA1</th>
+			<th>Includes</th>
+		</tr>
+		{{$dname := .Name}}
+		{{range .IDLModules}}
+		<tr>
+			<td><a href="{{$dname}}/{{.FilePath}}">{{.FilePath}}</a></td>
+			<td>{{.SHA1}}</td>
+			<td>
+			{{ range .Includes }}
+				<a href="{{$dname}}/{{.FilePath}}">{{.FilePath}}</a>
+			{{ end }}
+			</td>
+		</tr>
+		{{end}}
+	</table>
+{{end}}
+{{end}}
+`,
+}
